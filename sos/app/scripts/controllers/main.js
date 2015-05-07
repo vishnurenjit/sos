57ad58d42e51f0166c16ec17f29ff5409d86be88
Vishnu Renjit <vishnu.renjit@nibodha.com>
2015-05-07 12:09:30 +0530

    Add user UI

--- sos/app/scripts/controllers/main.js
+++ sos/app/scripts/controllers/main.js
@@ -9,11 +9,32 @@
  */
 angular.module('sos00App')
   .controller('MainCtrl', function ($scope) {
+    $scope.user = {};
   	$scope.logout = function() {
         Parse.User.logOut();
         // $rootScope.user = null;
         // $rootScope.isLoggedIn = false;
         alert("logOut");
-        $location.path("/login");
+
     };
+
+    $scope.loadModal = function() {
+    	console.log("Add user clicked");
+    	
+    	$('#addUser').modal('show');
+    }
+
+    $scope.addUser = function() {
+      var user = $scope.user;
+
+    }
+
+    var generatePassword = function() {
+      return "pass"
+    }
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
   });
